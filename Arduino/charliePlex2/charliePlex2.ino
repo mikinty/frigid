0fe9a0ef9e482365aee68eccfd9840f41559f78e
mikinty <myou@andrew.cmu.edu>
2017-11-11 14:46:42 -0500

    made a charlie plexing prototype for 4 LEDs, unfortunately I don't think this is going to our longterm solution since we need to do a refresh rate trick

--- Arduino/charliePlex2/charliePlex2.ino
+++ Arduino/charliePlex2/charliePlex2.ino
@@ -17,16 +17,19 @@
  * 
  */
 
-int pins = {2, 3, 4, 5};
+int pins[4] = {2, 3, 4, 5};
 int numPins = 4;
 
 // the LED ordering
-int LED = {
+int LED[12][2] = {
   {2, 3}, {2, 4}, {2, 5},
   {3, 2}, {3, 4}, {3, 5},
   {4, 2}, {4, 3}, {4, 5},
   {5, 2}, {5, 3}, {5, 4}
 };
+int numLED = 12;
+
+int DELAY = 600;
 
 void setup() {
   // put your setup code here, to run once:
@@ -36,13 +39,12 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(2, HIGH);
-  digitalWrite(3, LOW);
-  delay(500);
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-  delay(500);
+  // safety reasons yo
+  reset();
+  
+  for (int x = 0; x < numLED; x ++) {
+    turnOn(LED[x][0], LED[x][1]);      
+  }
 }
 
 /* reset turns all inputs into INPUT mode, called before 
@@ -50,15 +52,27 @@ void loop() {
  */
 void reset() {
   for (int x = 0; x < numPins; x++) {
-    digitalWrite(pins[x], INPUT); // set to input
+    pinMode(pins[x], INPUT); // set to input
   }
 }
 
 /* turnOn turns on the LED specified by the xy ordering
  *  given in the description above.
  */
-void turnOn(x, y) {
+void turnOn(int x, int y) {
+  // output mode
+  pinMode(pins[x], OUTPUT);
+  pinMode(pins[y], OUTPUT);
+
+  // turn LEDs on
   digitalWrite(pins[x], HIGH);
   digitalWrite(pins[y], LOW);
+
+  // wait
+  delay(DELAY);
+
+  // turn LEDs off
+  pinMode(pins[x], INPUT);
+  pinMode(pins[y], INPUT);
 }
 
