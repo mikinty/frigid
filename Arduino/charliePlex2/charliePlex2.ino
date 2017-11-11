06073284b70d9631149c05cf4b6a13fb0de6589d
mikinty <myou@andrew.cmu.edu>
2017-11-11 14:28:46 -0500

    some more setup for the 4-charliePlex

--- Arduino/charliePlex2/charliePlex2.ino
+++ Arduino/charliePlex2/charliePlex2.ino
@@ -1,7 +1,38 @@
+/*
+ * README!
+ * 
+ * LED Ordering (use 0-index in code):
+ * 12
+ * 13
+ * 14
+ * 21
+ * 23
+ * 24
+ * 31
+ * 32
+ * 34
+ * 41
+ * 42
+ * 43
+ * 
+ */
+
+int pins = {2, 3, 4, 5};
+int numPins = 4;
+
+// the LED ordering
+int LED = {
+  {2, 3}, {2, 4}, {2, 5},
+  {3, 2}, {3, 4}, {3, 5},
+  {4, 2}, {4, 3}, {4, 5},
+  {5, 2}, {5, 3}, {5, 4}
+};
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  for (int x = 0; x < numPins; x++) {
+    pinMode(pins[x], INPUT);
+  }
 }
 
 void loop() {
@@ -13,3 +44,21 @@ void loop() {
   digitalWrite(3, HIGH);
   delay(500);
 }
+
+/* reset turns all inputs into INPUT mode, called before 
+ *  lighting up any other LEDs
+ */
+void reset() {
+  for (int x = 0; x < numPins; x++) {
+    digitalWrite(pins[x], INPUT); // set to input
+  }
+}
+
+/* turnOn turns on the LED specified by the xy ordering
+ *  given in the description above.
+ */
+void turnOn(x, y) {
+  digitalWrite(pins[x], HIGH);
+  digitalWrite(pins[y], LOW);
+}
+
