11ef1b41bde0169c4da9323f319a9aa28d8062cb
mikinty <myou@andrew.cmu.edu>
2017-11-12 07:35:04 +0000

    ad

--- Arduino/test/test.ino
+++ Arduino/test/test.ino
@@ -20,22 +20,16 @@
 
 AccelStepper stepper1(HALFSTEP, motorPin2, motorPin4, motorPin3, motorPin5);
 AccelStepper stepper2(HALFSTEP, motorPin6, motorPin8, motorPin7, motorPin9);
-AccelStepper stepper3(HALFSTEP, motorPin10, motorPin12, motorPin11, motorPin13);
 
 void setup() {
-  Serial.begin(9601);
   stepper1.setMaxSpeed(1000.0);
   stepper1.setAcceleration(100.0);
   stepper1.setSpeed(200);
-  stepper1.moveTo(20000);
+  stepper1.moveTo(5000);
   stepper2.setMaxSpeed(1000.0);
   stepper2.setAcceleration(100.0);
   stepper2.setSpeed(200);
-  stepper2.moveTo(20000);
-  stepper3.setMaxSpeed(1000.0);
-  stepper3.setAcceleration(100.0);
-  stepper3.setSpeed(200);
-  stepper3.moveTo(20000);
+  stepper2.moveTo(5000);
 }
 
 void loop() {
@@ -46,11 +40,6 @@ void loop() {
   if (stepper2.distanceToGo() == 0){
     stepper2.moveTo(-stepper2.currentPosition());
   }
-
-  if (stepper3.distanceToGo() == 0){
-    stepper3.moveTo(-stepper3.currentPosition());
-  }
   stepper1.run();
   stepper2.run();
-  stepper3.run();
 }
