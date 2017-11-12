83e4d482590080931dd0da026281ca10cc913fa0
mikinty <myou@andrew.cmu.edu>
2017-11-12 02:22:37 +0000

    better compvi

--- Arduino/arduino2.py
+++ Arduino/arduino2.py
@@ -1,8 +1,13 @@
 import serial
+import time
 
 arduinoSerialData = serial.Serial('/dev/ttyACM2', 9600)
 
 while 1:
     if (arduinoSerialData.inWaiting() > 0):
-        myData = arduinoSerialData.readline()
-        print(myData)
+        # do something
+        
+        # myData = arduinoSerialData.readline()
+        print('asdf')
+        time.sleep(1)
+        
--- CompVision/testImage.py
+++ CompVision/testImage.py
@@ -1,6 +1,7 @@
 ########### Python 3.6 #############
 import http.client, urllib.request, urllib.parse, urllib.error, base64, json
 import subprocess
+import serial
 
 subscription_key = 'e282ecca3c86426eb5b416f9a5775f12'
 
@@ -16,6 +17,11 @@ params = urllib.parse.urlencode({
     'visualFeatures': 'Categories,Description,Color',
     'language': 'en',
 })
+
+arduinoSerialData = serial.Serial('/dev/ttyACM2', 9600)
+
+dic = ["bottle", "apple", "orange", "banana", "chips", "carton", "meat"]
+
 def identifyImage():
     req_body = open(filename, 'rb').read()
     try:
@@ -24,8 +30,15 @@ def identifyImage():
         response = conn.getresponse()
         data = response.read()
         parsed = json.loads(data.decode())
-    
-        print(parsed["description"]["tags"][0])
+
+        tags = parsed["description"]["tags"]
+        for i in range(len(tags)):
+            #print(tags[i])
+            if tags[i] in dic:
+                print(tags[i])
+                break
+        else:
+            print("Couldn't identify image. Closest match: ", tags[0])
         conn.close()
 
     except Exception as e:
@@ -33,8 +46,11 @@ def identifyImage():
         print(e)
 
 def m():
-    subprocess.check_output(["raspistill", "-o", "img.jpg"])
-    identifyImage()
+    while(1):
+        if (arduinoSerialData.inWaiting() > 0):
+            arduinoSerialData.readline()
+            subprocess.check_output(["raspistill", "-o", "img.jpg"])
+            identifyImage()
 
 
 m()
